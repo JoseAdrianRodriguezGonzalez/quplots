f38946e52c1049b4d4c08d37fc59d3f277ed6f61
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2026-09-07 03:20:12 -0600

    new version

--- src/quplots/__about__.py
+++ src/quplots/__about__.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2025-present Adrian <kerpador@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-#__version__ = "0.0.2a4"
-__version__ = "0.0.1"
+# __version__ = "0.0.2a4"
+__version__ = "0.0.3"
